a9c01e4f7e85a11c198032104605c77cef3a3207
wcombass99 <70278459+wcombass99@users.noreply.github.com>
2020-10-25 23:36:35 -0400

    Challenge commit

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+public Transform startMarker;
+public Transform endMarker;
+public float speed = 1.0F;
+private float startTime;
+private float journeyLength;
+
+void Start()
+     {
+          startTime = Time.time;
+
+          journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+     }
+
+void Update()
+     {
+          float distCovered = (Time.time - startTime) * speed;
+
+          float fracJourney = distCovered / journeyLength;
+
+          transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong (fracJourney, 1));
+     }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,26 +5,64 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text winText;
+    public Text lives;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    private bool facingRight = true;
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    
+    Animator anim; 
+   
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
+        winText.text = "";
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+        void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
+            if (facingRight == false && hozMovement > 0)
+            {
+                Flip();
+            }
+            else if (facingRight == true && hozMovement < 0)
+            {
+                Flip();
+            }
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        anim.SetInteger("State", 0);
+
+        if (hozMovement !=0)
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (verMovement !=0)
+        {
+        anim.SetInteger("State", 2);
+        }
 
         if (Input.GetKey("escape"))
         {
@@ -32,6 +70,13 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Coin")
@@ -39,6 +84,35 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        
+            if (scoreValue == 9)
+            {
+                winText.text = "You Win! Game by: Will Combass";
+                {
+                    musicSource.clip = musicClipTwo;
+                    musicSource.Play();
+                }
+            }
+
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2 (45.0f, 0);
+                livesValue = 3;
+                lives.text = livesValue.ToString();
+            }
+        }
+
+        if(collision.collider.tag == "Enemy")
+        {
+            livesValue -=1;
+            lives.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+
+            if (livesValue == 0)
+            {
+                Destroy (this.gameObject);
+                winText.text = "You Lose! Game by: Will Combass";
+            }
         }
     }
 
@@ -51,5 +125,14 @@ public class PlayerScript : MonoBehaviour
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
         }
+
+        if (collision.collider.tag == "Ground" && isOnGround)
+        {
+        if (Input.GetKey(KeyCode.W))
+        {
+        rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+        }
+        }
+        
     }
 }
